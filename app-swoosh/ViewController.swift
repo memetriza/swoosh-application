05e6b931343aa66a0b602c8558e77b702c6eaad5
Memet RIZA <memetriza@Memets-iMac.local>
2018-01-31 17:32:51 +0200

    First Commit1111

---
+++ app-swoosh/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Memet RIZA on 31/01/2018.
+//  Copyright © 2018 Memet RIZA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var swoosh: UIImageView!
+    
+    @IBOutlet weak var bgImg: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+        bgImg.frame = view.frame;
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
