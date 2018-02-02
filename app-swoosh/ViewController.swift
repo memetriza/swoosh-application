249a1cc6786be6edf4042317c19cbabe2226c6ef
Memet RIZA <memetriza@Memets-iMac.local>
2018-02-02 03:03:13 +0200

    Added new screens and seguways

--- app-swoosh/ViewController.swift
+++ app-swoosh/ViewController.swift
@@ -10,14 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var swoosh: UIImageView!
-    
     @IBOutlet weak var bgImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue){
         
     }
 
